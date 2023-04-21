36b53b382ad02f308bdc96cb82993ecc32e26c47
DangThaiKe <96134585+DangThaiKe@users.noreply.github.com>
2023-04-21 22:57:30 +0700

    Upload product-details

---
+++ CDWeb-k/src/main/java/com/example/CDWeb/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.CDWeb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String homePage() {
+		return "index";
+	}
+	@GetMapping("/product")
+	public String produc() {
+		return "product-details";
+	}
+}
